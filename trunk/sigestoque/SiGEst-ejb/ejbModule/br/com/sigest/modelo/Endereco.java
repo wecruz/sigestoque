9087422b7e57253ceb4e5c10e961f660339d27b9
SiGEstoque@gmail.com <SiGEstoque@gmail.com@3ac0f959-fa4e-13e7-0abf-288cce74000c>
2014-12-04 12:42:13 +0000

    adicionado a pagina funcionarios

---
+++ trunk/sigestoque/SiGEst-ejb/ejbModule/br/com/sigest/modelo/Endereco.java
@@ -0,0 +1,106 @@
+package br.com.sigest.modelo;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name ="tb_endereco")
+public class Endereco {
+
+	@Id
+	@GeneratedValue(strategy=GenerationType.AUTO)
+	@Column(name = "id_endereco", nullable = false)
+	private Long id;
+	
+	@Column(name = "endereco")
+	private String  endereco ;
+	
+	@Column(name = "cidade")
+	private String cidade ;
+	
+	@Column(name = "codigo")
+	private String codigo;
+	
+	@Column(name = "complemento")
+	private String complemento;
+	
+	@Column(name = "email")
+	private String email;
+	
+	@Column(name = "estado")
+	private String estado;
+	
+	@Column(name = "telefone")
+	private Integer telefone;
+
+	
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getEndereco() {
+		return endereco;
+	}
+
+	public void setEndereco(String endereco) {
+		this.endereco = endereco;
+	}
+
+	public String getCidade() {
+		return cidade;
+	}
+
+	public void setCidade(String cidade) {
+		this.cidade = cidade;
+	}
+
+	public String getCodigo() {
+		return codigo;
+	}
+
+	public void setCodigo(String codigo) {
+		this.codigo = codigo;
+	}
+
+	public String getComplemento() {
+		return complemento;
+	}
+
+	public void setComplemento(String complemento) {
+		this.complemento = complemento;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getEstado() {
+		return estado;
+	}
+
+	public void setEstado(String estado) {
+		this.estado = estado;
+	}
+
+	public Integer getTelefone() {
+		return telefone;
+	}
+
+	public void setTelefone(Integer telefone) {
+		this.telefone = telefone;
+	}
+	
+	
+}
